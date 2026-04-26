93419fd498e8e1e7b26da5cc3a20bb218dd6eb41
batya taieb <batia15777@icloud.com>
2026-04-27 01:40:16 +0300

    api

--- src/service/AuthApi.js
+++ src/service/AuthApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://localhost:8085",
 });
 
 export const loginEmployee = (data) => {
