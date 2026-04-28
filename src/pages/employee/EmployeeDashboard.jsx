a6743c89bd875e939f09b654602364867cd0f471
batya taieb <batia15777@icloud.com>
2026-04-29 01:19:10 +0300

    .Frontend: Implemented EmployeeDashboard screen (Batya)
     - Added state management for work status (isWorking, startTime, location)
     - Implemented Enter/Exit button logic based on current work status
     - Integrated WorkTimer component to display live working time
     - Connected to Work API (enter, exit, get status)
     - Handles UI updates after user actions
    
     Author: Batya

--- src/pages/employee/EmployeeDashboard.jsx
+++ src/pages/employee/EmployeeDashboard.jsx
@@ -1,10 +1,76 @@
-function EmployeeDashBoard() {
+import {useEffect, useState} from "react";
+import Cookies from "js-cookie";
+import {enterWork, exitWork, getCurrentWorker} from "../../service/WorkApi.js";
+import LocationButton from "../../components/LocationButton.jsx";
+import NavigationTabs from "../../components/NavigationTabs.jsx";
+import WorkTimer from "../../components/WorkTimer.jsx";
 
+function EmployeeDashboard() {
+
+    const [location, setLocation] = useState("office");
+    const [isWorking, setIsWorking] = useState(false);
+    const [startTime, setStartTime] = useState(null);
+    const token = Cookies.get("token");
+
+
+    useEffect(() => {
+        // לסנכרן עם רומן את המשתנים שהיהה אותם שמות כמו בצד שרת כלומר מה שחוזר מהשרת
+        getCurrentWorker(token)
+            .then(response => {
+                if (response.data.success){
+                    setIsWorking(response.data.isWorking)
+                    setStartTime(response.data.startTime);
+                }
+            }).catch(()=>console.log("error"))
+
+    },[])
+
+    const handleEnter =()=>{
+       const now = new Date();
+       console.log("CLICK ENTER")
+        const data = {
+            token: token,
+            location: location,
+            startTime: now,
+        };
+        enterWork(data)
+            .then(response => {
+                if (response.data.success){
+                    setIsWorking(true);
+                    setStartTime(now);
+                }
+            }).catch(()=>console.log("error"))
+    }
+
+
+    const handleExit =()=>{
+        const now = new Date();
+        const data = {
+            token: token,
+            location: location,
+            endTime: now,
+        };
+        exitWork(data)
+            .then(response => {
+                if (response.data.success){
+                    setIsWorking(false);
+                    setStartTime(null);
+                }
+            }).catch(()=>console.log("error"))
+    }
 
    return (
        <>
-           EmployeeDashBoard
+          <NavigationTabs active={"dashboard"}/>
+           <LocationButton location={location} setLocation={setLocation}/>
+           <WorkTimer isWorking={isWorking} startTime={startTime}/>
+           <button onClick={isWorking ? handleExit:handleEnter}>
+               {isWorking?"Exit":"Enter"}
+           </button>
+           <button> Report Absence </button>
+
+
        </>
    )
 }
-export default EmployeeDashBoard;
+export default EmployeeDashboard;
