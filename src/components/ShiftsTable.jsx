40078a0ba8c3d6b1e21d21c5ab3c09f87ec759d1
karinak2001 <karinak2001@edu.aac.ac.il>
2026-05-17 13:57:27 +0300

    Added NavbarEmployee to EmployeeShiftsPage, added fields to ShiftsTable, and fixed spelling mistakes.

--- src/components/ShiftsTable.jsx
+++ src/components/ShiftsTable.jsx
@@ -1,30 +1,37 @@
-function SiftsTable({shifts}) {
+function ShiftsTable({shifts}) {
 
     return (
         <table>
             <thead>
                 <tr>
+                     <th>Day</th>
                      <th>Date</th>
                      <th>Start Hour</th>
                      <th>End Hour</th>
+                     <th>Location</th>
+                     <th>Role</th>
                 </tr>
             </thead>
+
             <tbody>
 
             {shifts.map((shift, index) => (
 
                 <tr key={index}>
 
+                    <td>{shift.day}</td>
                     <td>{shift.date}</td>
                     <td>{shift.startHour}</td>
                     <td>{shift.endHour}</td>
+                    <td>{shift.location}</td>
+                    <td>{shift.role}</td>
 
                 </tr>
-                ))}
+            ))}
             </tbody>
 
         </table>
     )
 }
 
-export default SiftsTable;
+export default ShiftsTable;
